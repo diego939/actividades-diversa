a6765cff84d91630c5852bfdd33aabda3038bcb4
DiegoDavid17 <diegodavidalmiron@gmail.com>
2024-04-05 18:10:30 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function ocultar(){
+    document.getElementById('demo').style.visibility = "hidden";
+    }
+
+    function mostrar(){
+        document.getElementById('demo').style.visibility = "visible";
+        }
