e0b9ad42428fda85e1606de67a2e81adb15379e3
DiegoDavid17 <diegodavidalmiron@gmail.com>
2024-04-10 21:40:09 -0300

    alerta

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+
+
 function ocultar(){
     document.getElementById('demo').style.visibility = "hidden";
     }
 
     function mostrar(){
         document.getElementById('demo').style.visibility = "visible";
+        }
+        function inscripcion(){ 
+            Swal.fire({
+            title: "<strong>Cuando tengamos el formulario de inscripción lo publicaremos aquí</strong>",
+            icon: "info",
+            showCloseButton: true,
+            focusConfirm: false,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: `
+                <i class=""></i> ok!
+            `,
+            });
         }
